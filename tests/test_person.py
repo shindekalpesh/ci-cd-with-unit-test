fe1c14fbec7244186ef31691596312ad9be60282
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2026-04-07 18:50:12 +0530

    added one test: test_init() which ran successfully

---
+++ tests/test_person.py
@@ -0,0 +1,7 @@
+from src.person import Person
+
+def test_init():
+    person = Person("Thomas Shelby", 34, jobs=["Software Developer"])
+    assert person.name == "Thomas Shelby"
+    assert person.age == 34
+    assert person.jobs == ["Software Developer"]
