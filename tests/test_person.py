03b379a0eb0b5db207caf7a4cd3eff646ac460f9
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2026-04-08 07:19:34 +0530

    added fixture person to avoid repeating it every test func. Note: while calling the fixture, a new object is created everytime of the fixture and not the same instance is executed.

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,7 +1,14 @@
 from src.person import Person
 
+import pytest
+
+@pytest.fixture()
+def person():
+    return Person("Thomas Shelby", 34, jobs=["Software Developer"])
+    
+
 def test_init():
-    person = Person("Thomas Shelby", 34, jobs=["Software Developer"])
+    person = 
     assert person.name == "Thomas Shelby"
     assert person.age == 34
     assert person.jobs == ["Software Developer"]
