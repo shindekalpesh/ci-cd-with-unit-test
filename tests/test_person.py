dc919c2223ef727e3cc71d84400ec78bdb75bd2a
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2026-04-08 07:12:35 +0530

    added another test for first_name, ran successfully

--- tests/test_person.py
+++ tests/test_person.py
@@ -5,3 +5,8 @@ def test_init():
     assert person.name == "Thomas Shelby"
     assert person.age == 34
     assert person.jobs == ["Software Developer"]
+
+
+def test_first_name():
+    person = Person("Thomas Shelby", 34, jobs=["Software Developer"])
+    assert person.first_name == "Thomas"
